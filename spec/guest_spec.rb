c48fca94658cba8c910766d572653b558bb167ef
leeburgess67 <leeburgessmail@gmail.com>
2018-06-01 11:40:38 +0100

    has money test passed

--- spec/guest_spec.rb
+++ spec/guest_spec.rb
@@ -5,11 +5,11 @@ require ("minitest/rg")
 class GuestTest < MiniTest::Test
 
   def setup
-    @guest1 = Guest.new("Axl Rose", "November Rain")
-    @guest2 = Guest.new("Bon Scott", "Highway to hell")
-    @guest3 = Guest.new("Ozzy Osbourne", "Children of the grave")
-    @guest4 = Guest.new("Rob Halford", "Painkiller")
-    @guest5 = Guest.new("Bruce Dickenson", "Run to the hills")
+    @guest1 = Guest.new("Axl Rose", "November Rain", 50)
+    @guest2 = Guest.new("Bon Scott", "Highway to hell", 5)
+    @guest3 = Guest.new("Ozzy Osbourne", "Children of the grave", 10)
+    @guest4 = Guest.new("Rob Halford", "Painkiller", 0)
+    @guest5 = Guest.new("Bruce Dickenson", "Run to the hills", 15)
   end
 
   def test_has_name
@@ -18,6 +18,10 @@ class GuestTest < MiniTest::Test
 
   def test_has_fav_song
     assert_equal("Highway to hell", @guest2.fav_song)
+  end
+
+  def test_has_money
+    assert_equal(10, @guest3.wallet)ß
   end
 
 
